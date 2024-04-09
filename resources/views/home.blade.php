5c8d913ffb6802803cd16efa60bf0422e49224c8
Luca <lucafagiolini@live.it>
2024-04-09 19:20:05 +0200

    exeercise completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,21 @@
 @extends ('layouts/app')
 
 @section('content')
-    @foreach ($movies as $movie)
-        <h2>
-            {{ $movie->title }}
-        </h2>
-    @endforeach
+    <div class="container d-flex flex-wrap flex-column align-content-center text-center mt-5">
+        <h1>movies</h1>
+        <div class="row gap-3 mt-3">
+
+            @foreach ($movies as $movie)
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <p class="card-text">{{ $movie->original_title }}</p>
+                        <h5 class="card-text">{{ $movie->date }}</h5>
+                        <h5 class="card-text">{{ $movie->vote }}</h5>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+    </div>
 @endsection
